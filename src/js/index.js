eea9beb052aff16838a886d8aae95b326a1942ba
keylperez <perez.williamcaleb@gmail.com>
2021-03-09 22:35:14 +0800

    products finishing

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@
 
 const Right = document.getElementById("arrowButt-right");
 const Left = document.getElementById("arrowButt-left");
+// var rightBtn = document.getElementsByClassName("arrow-icon-left");
+// var leftBtn = document.getElementsByClassName("arrow-icon-right");
 
 Right.addEventListener("click", goRight);
 Left.addEventListener("click", goLeft);
@@ -9,11 +11,19 @@ Left.addEventListener("click", goLeft);
 function goRight(){
     document.getElementById("grid-container-left").style.display = "none";
     document.getElementById("grid-container-right").style.display = "grid";
+    Right.style.opacity = 0.5;
+    Left.style.opacity = 1;
+    Right.style.cursor = "no-drop";
+    Left.style.cursor = "pointer";
     console.log("success");
 }
 
 function goLeft(){
     document.getElementById("grid-container-left").style.display = "grid";
     document.getElementById("grid-container-right").style.display = "none";
+    Right.style.opacity = 1;
+    Left.style.opacity = 0.5;
+    Right.style.cursor = "pointer";
+    Left.style.cursor = "no-drop";
     console.log("success");
 }
