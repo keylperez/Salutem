dff5b091511acbd858b366af47da0a41e5dbd2b2
keylperez <perez.williamcaleb@gmail.com>
2021-03-10 08:53:32 +0800

    Normal responsiveness

--- src/js/index.js
+++ src/js/index.js
@@ -2,28 +2,44 @@
 
 const Right = document.getElementById("arrowButt-right");
 const Left = document.getElementById("arrowButt-left");
-// var rightBtn = document.getElementsByClassName("arrow-icon-left");
-// var leftBtn = document.getElementsByClassName("arrow-icon-right");
+var x = 0;
 
 Right.addEventListener("click", goRight);
 Left.addEventListener("click", goLeft);
 
 function goRight(){
-    document.getElementById("grid-container-left").style.display = "none";
-    document.getElementById("grid-container-right").style.display = "grid";
-    Right.style.opacity = 0.5;
-    Left.style.opacity = 1;
-    Right.style.cursor = "no-drop";
-    Left.style.cursor = "pointer";
-    console.log("success");
+    if  (x == 0){
+        document.getElementById("grid-container-left").style.display = "none";
+        document.getElementById("grid-container-right").style.display = "grid";
+        Right.style.opacity = 0.5;
+        Left.style.opacity = 1;
+        Right.style.cursor = "no-drop";
+        Left.style.cursor = "pointer";
+        document.location = "#products";
+        console.log("success");
+        x = 1;
+    }
+    else{
+        console.log("error");
+    }
+
 }
 
 function goLeft(){
-    document.getElementById("grid-container-left").style.display = "grid";
-    document.getElementById("grid-container-right").style.display = "none";
-    Right.style.opacity = 1;
-    Left.style.opacity = 0.5;
-    Right.style.cursor = "pointer";
-    Left.style.cursor = "no-drop";
-    console.log("success");
+    if (x == 1) {
+        document.getElementById("grid-container-left").style.display = "grid";
+        document.getElementById("grid-container-right").style.display = "none";
+        Right.style.opacity = 1;
+        Left.style.opacity = 0.5;
+        Right.style.cursor = "pointer";
+        Left.style.cursor = "no-drop";
+        document.location = "#products";
+        console.log("success");
+        x = 0;
+    }
+    else{
+        console.log("error");
+    }
+
+
 }
